c0696385fab88d6e9c942ffc59a65523e9928479
Very lixo <ryofreefire32@gmail.com>
2025-11-20 14:43:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -62,70 +62,67 @@ def index():
 </head>
 <body>
 
-    <div class="container">
-        <h1>Carrier list</h1>
-        <h2>By Silva</h2>
-
-        <div class="sep"></div>
-
-        <h1>1. The Golden</h1>
+            <div class="sep"></div>
+        <h1><a href="/golden">1. The Golden</a></h1>
         <h2>by BoBoBoBoBoBoBo</h2>
 
         <div class="sep"></div>
-        <h1>2. Widestep</h1>
+        <h1><a href="/widestep">2. Widestep</a></h1>
         <h2>by Demishio</h2>
 
         <div class="sep"></div>
-        <h1>3. Cosmic Cyclone</h1>
+        <h1><a href="/CosmicCy">3. Cosmic Cyclone</a></h1>
         <h2>by APTeamOfficial</h2>
 
         <div class="sep"></div>
-        <h1>4. The Hell Zone</h1>
+        <h1><a href="/thz">4. The Hell Zone</a></h1>
         <h2>by Stormfly</h2>
 
         <div class="sep"></div>
-        <h1>5. Blade of Justice</h1>
+        <h1><a href="/bj">5. Blade of Justice</a></h1>
         <h2>by Manix648</h2>
 
         <div class="sep"></div>
-        <h1>6. moment</h1>
+        <h1><a href="/moment">6. moment</a></h1>
         <h2>by Lexycat</h2>
 
         <div class="sep"></div>
-        <h1>7. Bloodbath</h1>
+        <h1><a href="/bb">7. Bloodbath</a></h1>
         <h2>by Riot</h2>
 
         <div class="sep"></div>
-        <h1>8. The Hell Dignity</h1>
+        <h1><a href="/thd">8. The Hell Dignity</a></h1>
         <h2>by Stormfly</h2>
 
         <div class="sep"></div>
-        <h1>9. Reflective</h1>
+        <h1><a href="/reflective">9. Reflective</a></h1>
         <h2>by Mojitoz</h2>
 
         <div class="sep"></div>
-        <h1>10. NecropoliX</h1>
+        <h1><a href="/necropolix">10. NecropoliX</a></h1>
         <h2>by namtar</h2>
 
         <div class="sep"></div>
-        <h1>11. The Lost Existence</h1>
+        <h1><a href="/tle">11. The Lost Existence</a></h1>
         <h2>by JonathanGD</h2>
 
         <div class="sep"></div>
-        <h1>12. Cataclysm</h1>
+        <h1><a href="/cata">12. Cataclysm</a></h1>
         <h2>by Ggb0y</h2>
 
         <div class="sep"></div>
-        <h1>13. Niwa</h1>
+        <h1><a href="/niwa">13. Niwa</a></h1>
         <h2>by Teno</h2>
 
         <div class="sep"></div>
-        <h1>14. Acu</h1>
+        <h1><a href="/acu">14. Acu</a></h1>
         <h2>by neigefeu</h2>
 
         <div class="sep"></div>
-        <h1>15. ICE Carbon Diablo X</h1>
+        <h1><a href="/ICDX">15. ICE Carbon Diablo X</a></h1>
         <h2>by -Roadbose-</h2>
+
+        <!-- A PARTIR DAQUI NÃO TEM ENDPOINT, ENTÃO SEM LINK -->
 
         <div class="sep"></div>
         <h1>16. CraZy III</h1>
@@ -269,7 +266,367 @@ def index():
 
         <div class="sep"></div>
 
-        <h3><a href="/info">Info</a></h3>
+        <h3><a href="/info">Info</a> <a href="https://www.youtube.com/@anonimorandom">Carrier</a></h3>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/golden")
+def index1():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#1 - The Golden - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>The Golden</h1>
+        <p>By BoBoBoBoBoBoBo, Verified by nSwish</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/SJ7SIDO5sj4"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/widestep")
+def wide():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#2 - Widestep - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Widestep</h1>
+        <p>By 出見塩</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/TN8fHN_XV3o"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/CosmicCy")
+def Cosmic():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#3 - Cosmic Cyclone - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Cosmic Cyclone</h1>
+        <p>By APTeamOfficial, Verified by DoSh7t</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/MdCwu0W8"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/thz")
+def thz():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#4 - The Hell Zone - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>The Hell Zone</h1>
+        <p>By Koreaqwer, Verified by Stormfly</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/6ebIqtnGpdo"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/bj")
+def bj():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#5 - Blade of Justice - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Blade of Justice</h1>
+        <p>By Manix648, Verified by RicoLP</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/pQc8KL-n20Y"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/moment")
+def moment():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#6 - moment - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>moment</h1>
+        <p>By lexy, Verified by icedcave</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/bLfKsN4NCNY"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/bb")
+def bb():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#7 - Bloodbath - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Bloodbath</h1>
+        <p>By Riot, Verified by Riot</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/twTw4fjT0ik"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/thd")
+def thd():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#8 - The Hell Dignity - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>The Hell Dignity</h1>
+        <p>By Sohn0924, Verified by Stormfly</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/nQuVjZ11Lyw"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/reflective")
+def reflective():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#9 - Reflective - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Reflective</h1>
+        <p>By Mojitoz, Verified by Vorgogne</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/1fzhBT_7Fos"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/necropolix")
+def necropolix():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#10 - NecropoliX - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>NecropoliX</h1>
+        <p>By Namtar, Verified by Trusta</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/oV9Oj7YyhpQ"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/tle")
+def tle():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#11 - The Lost Existence - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>The Lost Existence</h1>
+        <p>By JonathanGD, Verified by Luqualizer</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/XztPxRuKPi4"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/cata")
+def cata():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#12 - Cataclysm - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Cataclysm</h1>
+        <p>By Ggb0y, Verified by Riot</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/vGV4j8C66JY"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/niwa")
+def niwa():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#13 - Niwa - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Niwa</h1>
+        <p>By Teno, Verified by Nicor77</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/RXf4PwKRo"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/acu")
+def acu():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#14 - Acu - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Acu</h1>
+        <p>By neigefeu, Verified by neigefeu</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/z6l74Mkoxm8"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
+    </div>
+
+</body>
+</html>"""
+
+@app.route("/ICDX")
+def ICDX():
+    return """<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#14 - Ice Carbon Diablo X - Carrier List</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="level">
+        <h1>Ice Carbon Diablo X</h1>
+        <p>By roadbose, Verified by Riot</p>
+        <iframe width="560" height="315"
+    src="https://www.youtube.com/embed/ykwlYeTJblc"
+    frameborder="0"
+    allowfullscreen>
+</iframe>
     </div>
 
 </body>
@@ -277,5 +634,7 @@ def index():
 
 @app.route("/info")
 def info():
-    return """<p><h1>this website was made by: ryofreefire32@gmail.com (Silva)</h1></p>
+    return """<p><h1>this website was made by: ryofreefire32@gmail.com (Silva) and @yuridakbrada</h1></p>
 <p><h2><a href="/">Back</a></h2></p>"""
+
+app.run()
